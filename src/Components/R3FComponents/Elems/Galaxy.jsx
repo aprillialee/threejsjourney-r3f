ee9940130cce5b4a8b14687ea0c26de1c00d9342
aprillialee <aprilliapmlee@gmail.com>
2022-02-21 17:00:57 +0000

    added randomness to the particles

--- src/Components/R3FComponents/Elems/Galaxy.jsx
+++ src/Components/R3FComponents/Elems/Galaxy.jsx
@@ -11,17 +11,21 @@ const Galaxy = () => {
   const bufferRef = useRef();
 
   // Leva Controls
-  const { count, size, radius, sizeAttenuation, set } = useControls({
-    count: { value: 100, min: 0, max: 3000, step: 10 },
-    size: {
-      value: 0.03,
-      min: 0.1,
-      max: 2,
-      step: 0.1,
-    },
-    radius: { value: 2, min: 1, max: 20, step: 0.01 },
-    sizeAttenuation: true,
-  });
+  const { count, size, radiusV, branches, spin, randomness, randomnessPower } =
+    useControls({
+      count: { value: 5000, min: 0, max: 10000, step: 10 },
+      size: {
+        value: 0.02,
+        min: 0.01,
+        max: 1,
+        step: 0.01,
+      },
+      radiusV: { value: 2, min: 1, max: 20, step: 0.01 },
+      branches: { value: 3, min: 1, max: 20, step: 1 },
+      spin: { value: 0.2, min: 0, max: 5, step: 0.2 },
+      randomness: { value: 0.2, min: 0, max: 5, step: 0.1 },
+      randomnessPower: { value: 3, min: 1, max: 10, step: 0.01 },
+    });
 
   // Generate particles
   const [positions] = useMemo(() => {
@@ -30,16 +34,30 @@ const Galaxy = () => {
     for (let i = 0; i < count; i++) {
       const i3 = i * 3;
 
-      const radius1 = Math.random() * radius;
+      const radius = Math.random() * radiusV;
+      const spinAngle = radius * spin;
+      const branchAngle = ((i % branches) / branches) * Math.PI * 2;
 
-      positions[i3] = radius1;
-      positions[i3 + 1] = 0;
-      positions[i3 + 2] = 0;
+      const randomX =
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1);
+      const randomY =
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1);
+      const randomZ =
+        Math.pow(Math.random(), randomnessPower) *
+        (Math.random() < 0.5 ? 1 : -1);
+
+      positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX;
+      positions[i3 + 1] = randomY;
+      positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ;
     }
     return [positions];
   });
 
+  // Update the buffer attribute array when changed by leva values - must clear the previous buffer attribute & create a new one to prevent a crash!
   useEffect(() => {
+    bufferRef.current.dispose();
     bufferRef.current.setAttribute(
       "position",
       new BufferAttribute(positions, 3)
@@ -58,7 +76,7 @@ const Galaxy = () => {
       </bufferGeometry>
       <pointsMaterial
         size={size}
-        sizeAttenuation={sizeAttenuation}
+        sizeAttenuation={true}
         color={"white"}
         blending={AdditiveBlending}
       />
